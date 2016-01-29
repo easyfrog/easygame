913e6835219eaf4e81975e08e754d6226735c0a1
easyfrog <easyfrog@126.com>
2016-01-29 18:14:54 +0800

    add utils.coroutine function. to control the action flow like in Unity3D.

--- js/webShow/utils.js
+++ js/webShow/utils.js
@@ -399,3 +399,103 @@ if (typeof String.prototype.endsWith != 'function') {
         return this.indexOf(suffix, this.length - suffix.length) !== -1;
     };
 }
+
+/**
+ * 模拟 Unity3D C# 中的 coroutine
+ * var cor = utils.coroutine();
+ * cor.push(fun (on complete add cor.workComplete()), cor.wait(2), fun ...);
+ * cor.next(); 	// for one by one 
+ * cor.start();	// for sequence
+ * cor.stop();
+ * cor.resume();
+ * cor.reset();
+ */
+ utils.coroutine = function() {
+    return (new function() {
+        // 任务组
+        this.works        = [];
+        this.isWorking    = false;
+        this.done         = false;
+        this.doneCallback = null;
+        this.index        = -1;
+        this.isStop       = false;
+
+        // 添加任务
+        this.push = function(works) {
+            var w = arguments;
+            for (var i = 0; i < w.length; i++) {
+                this.works.push( w[i] );
+            };
+        };
+
+        // 下一个
+        this.next = function() {
+        	var s = this;
+            if (s.isWorking) {
+                return;
+            }
+            s.index += 1;
+            var w = s.works[s.index];
+            if (w) {
+                s.isWorking = true;
+                s.done = false;
+                w(s.workComplete.bind(s));
+            }
+        };
+
+        // 完成work后,调用的方法
+        this.workComplete = function( next ) {
+            this.isWorking = false;
+
+            if (next == undefined) {
+                next = true;
+            }
+            
+            if (this.index == this.works.length - 1) {
+                this.done = true;
+                this.index = -1;
+                if (this.doneCallback) {
+                    this.doneCallback();
+                }
+            } else if (next) {
+                if (!this.isStop) {
+                    this.next();
+                }
+            }
+        };
+
+        // wait for seconds
+        this.wait = function( seconds ) {
+            var s = this;
+            return function() {
+                setTimeout(function() {
+                    s.workComplete();
+                }, seconds * 1e3);
+            }
+        };
+
+        // 开始序列
+        this.start = function() {
+            this.next();
+        };
+
+        // 停止序列
+        this.stop = function() {
+            this.isStop = true;
+        };
+
+        // 恢复序列
+        this.resume = function() {
+            this.isStop = false;
+            this.next();
+        };
+
+        // 清空
+        this.reset = function() {
+			this.index     = -1;
+			this.isWorking = false;
+			this.works     = [];
+			this.isStop    = false;
+        };
+    });
+};
